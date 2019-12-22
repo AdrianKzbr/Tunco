57402b39a1cdc11a4b35e4ae971863b92ab10120
Vinyks <54633020+Vinyks@users.noreply.github.com>
2019-12-22 17:53:27 +0100

    MODEL
    model

--- TuncoAlloys/src/main/java/com/Vinyks/Tunco/blocks/animation/ModelTungstenChest.java
+++ TuncoAlloys/src/main/java/com/Vinyks/Tunco/blocks/animation/ModelTungstenChest.java
@@ -3,10 +3,13 @@ package com.Vinyks.Tunco.blocks.animation;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-
+@SideOnly(Side.CLIENT)
 public class ModelTungstenChest extends ModelBase {
-    public ModelRenderer handle;
+   
+	public ModelRenderer handle;
     public ModelRenderer lid;
     public ModelRenderer storage;
 
@@ -24,16 +27,14 @@ public class ModelTungstenChest extends ModelBase {
         this.handle.addBox(-1.0F, -2.0F, -15.0F, 2, 4, 1, 0.0F);
     }
 
-    @Override
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
-        this.lid.render(f5);
-        this.storage.render(f5);
-        this.handle.render(f5);
+    public void renderAll()
+    {
+    	this.handle.rotateAngleX = this.lid.rotateAngleX;
+    	this.lid.render(0.0625f);
+    	this.handle.render(0.0625f);
+    	this.storage.render(0.0625f);
+    	
     }
-
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-        modelRenderer.rotateAngleX = x;
-        modelRenderer.rotateAngleY = y;
-        modelRenderer.rotateAngleZ = z;
-    }
+    
+    
 }
